8e5871e6b1fe5a2578dec7ab3d10e08692bde0fd
Martin Murphy <martin.murphy@whiteboard-it.com>
2011-06-29 23:52:01 -0500

    new tests

--- Mite.Test/MigrationHelperTestFixture.cs
+++ Mite.Test/MigrationHelperTestFixture.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Mite.Core;
+﻿using Mite.Core;
 using NUnit.Framework;
 
 namespace Mite.Test
@@ -15,18 +11,23 @@ namespace Mite.Test
         {
             
         }
-        
+              
         [Test]
-        public void UpMigrationWorksIfGreaterThanAllVersions()
+        public void UpMigrationWorksIfPartialDateString()
         {
-            var container = new MigrationContainer(new Migration("2011-01-05", MigrationType.Up, "1"), new Migration("2012-02-05", MigrationType.Up, "2"));
+            var container = new MigrationContainer(
+                new Migration("2011-01-05", MigrationType.Up, "1"), 
+                new Migration("2012-02-05", MigrationType.Up, "2"));
             var plan = container.GetMigrationPlan(null, "3000");
-            Assert.AreEqual(plan.SqlToExecute.Length, 2);
+            Assert.AreEqual(plan.SqlToExecute.Length, 2);            
         }
         [Test]
         public void UpMigrationsIgnoreDownMigrations()
         {
-            var container = new MigrationContainer(new Migration("2011-01-05", MigrationType.Up, "1"), new Migration("2012-02-05", MigrationType.Up, "2"), new Migration("2011-02-04", MigrationType.Down, "2"));
+            var container = new MigrationContainer(
+                new Migration("2011-01-05", MigrationType.Up, "1"), 
+                new Migration("2012-02-05", MigrationType.Up, "2"), 
+                new Migration("2011-02-04", MigrationType.Down, "2"));
             var plan = container.GetMigrationPlan(null, "3000");
             Assert.AreEqual(plan.SqlToExecute.Length, 2);
         }
@@ -38,11 +39,12 @@ namespace Mite.Test
                                  {
                                      new Migration("2011-01-05", MigrationType.Up, "1"),
                                      new Migration("2012-02-05", MigrationType.Up, "3"),
+                                     new Migration("2012-02-05T06-05-08Z", MigrationType.Up, "4"),
                                      new Migration("2011-02-04", MigrationType.Up, "2")
                                  };
             var container = new MigrationContainer(migrations);
             var plan = container.GetMigrationPlan(null, "3000");
-            Assert.AreEqual(plan.SqlToExecute , new[] {"1", "2", "3"});
+            Assert.AreEqual(plan.SqlToExecute , new[] {"1", "2", "3", "4"});
         }
     }
 }
