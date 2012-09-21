196eaf760a63a8bbb55e15bcd924a6b81eff1347
Martin Murphy <martin.murphy@whiteboard-it.com>
2012-09-20 22:06:59 -0500

    Added MsBuild Tasks:  MiteEnsure and MiteUpdate

---
+++ MsBuildTask/MiteEnsure.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Mite.Builder;
+
+namespace MsBuildTask {
+    public class MiteEnsure : Task {
+        public override bool Execute() {
+            var migrator = MigratorFactory.GetMigrator(ScriptsDirectory);
+            var result = migrator.Tracker.IsValidState();
+            if (result)
+            {
+                Log.LogMessage("Mite database status is good.");
+            }else
+            {
+                Log.LogError("Mite status is not clean");
+                foreach (var migration in migrator.Tracker.InvalidMigrations())
+                {
+                    Log.LogError("Mismatch checksum on: " + migration.Version +"[" + migration.Hash+ "]");
+                }
+            }
+            return result;
+        }
+
+        [Required]
+        public string ScriptsDirectory { get; set; }
+    }
+}
---
+++ MsBuildTask/MiteUpdate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Mite.Builder;
+
+namespace MsBuildTask {
+    public class MiteUpdate : Task {
+        public override bool Execute() {
+            try {
+                var migrator = MigratorFactory.GetMigrator(ScriptsDirectory);
+                var result = migrator.Update();
+                Log.LogMessage("Success: " + result.PriorToMigration +" to " + result.AfterMigration);
+                return true;
+            } catch (Exception ex) {
+                Log.LogError(ex.Message);
+                return false;
+            }
+        }
+        [Required]
+        public string ScriptsDirectory { get; set; }
+    }
+}
